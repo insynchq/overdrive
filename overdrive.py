2713183bd1a18e3ed15942ec2af8ff8bbf916832
Mark Steve Samson <marksteve@insynchq.com>
2013-08-17 12:46:04 +0800

    Initial commit

---
+++ overdrive.py
@@ -0,0 +1,13 @@
+import sublime, sublime_plugin
+
+
+class OverdriveShareCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        pass
+
+
+class OverdriveJoinCommand(sublime_plugin.ApplicationCommand):
+
+    def  run(self):
+        pass
