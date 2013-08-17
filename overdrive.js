d8c93f1e6c61ed640dd0039420c114d3c3a03717
Mark Steve Samson <marksteve@insynchq.com>
2013-08-17 18:00:13 +0800

    Fixed namespace issues
    --HG--
    extra : rebase_source : 56a9e52a4d97565a1459589513b64b09ec64b6b3

--- overdrive.js
+++ overdrive.js
@@ -1,117 +1,121 @@
-var CLIENT_ID = '849493785001.apps.googleusercontent.com';
+var Overdrive = {
 
-function initializeModel(model) {
-  var string = model.createString('Hello, world');
-  model.getRoot().set('text', string);
-}
+  CLIENT_ID: '849493785001.apps.googleusercontent.com',
 
-function send(o) {
-  alert(JSON.stringify(o));
-}
+  initializeModel: function(model) {
+    var string = model.createString('Hello, world');
+    model.getRoot().set('text', string);
+  },
 
-function sendEvent(e) {
-  send({
-    bubbles: e.bubbles,
-    isLocal: e.isLocal,
-    sessionId: e.sessionId,
-    type: e.type,
-    userId: e.userId,
-    index: e.index,
-    text: e.text
-  });
-}
+  send: function(o) {
+    alert(JSON.stringify(o));
+  },
 
-function onFileLoaded(doc) {
-  var string = doc.getModel().getRoot().get('text');
-  string.addEventListener(
-    gapi.drive.realtime.EventType.TEXT_INSERTED,
-    sendEvent
-  );
-  string.addEventListener(
-    gapi.drive.realtime.EventType.TEXT_DELETED,
-    sendEvent
-  );
-  gapi.drive.realtime.databinding.bindString(
-    string,
-    document.getElementById('editor')
-  );
-  send({
-    type: 'file_content_loaded',
-    text: string.getText()
-  });
-}
-
-function create(title, userId, accessToken) {
-  gapi.load('auth:client', function() {
-    gapi.auth.setToken({access_token: accessToken});
-    rtclient.createRealtimeFile(title, rtclient.REALTIME_MIMETYPE, function(file) {
-      if (file && file.id) {
-        open(file.id, userId, accessToken);
-      } else {
-        send({
-          type: 'error',
-          error: 'Failed to create file'
-        });
-      }
+  sendEvent: function(e) {
+    Overdrive.send({
+      bubbles: e.bubbles,
+      isLocal: e.isLocal,
+      sessionId: e.sessionId,
+      type: e.type,
+      userId: e.userId,
+      index: e.index,
+      text: e.text
     });
-  });
-}
+  },
 
-function open(fileId, userId, accessToken) {
-  var options = {
-    clientId: CLIENT_ID,
-    initializeModel: initializeModel,
-    autoCreate: false,
-    newFileMimeType: null, // Using default.
-    onFileLoaded: onFileLoaded,
-    registerTypes: null, // No action.
-    afterAuth: null // No action.
-  };
-
-  var rtLoader = new rtclient.RealtimeLoader(options);
-  // Setting this here because I'm too lazy to update realtime-client-utils.js
-  rtclient.params.fileIds = fileId;
-  rtclient.params.userId = userId;
-
-  gapi.load('auth:client,drive-realtime,drive-share', function() {
-    gapi.auth.setToken({access_token: accessToken});
-    rtLoader.load();
-    rtclient.getFileMetadata(fileId, function(metadata) {
-      if (metadata.error) {
-        send({
-          type: 'error',
-          error: 'File not found'
-        });
-      } else {
-        send({
-          type: 'file_metadata_loaded',
-          metadata: metadata
-        });
-      }
+  onFileLoaded: function(doc) {
+    var string = doc.getModel().getRoot().get('text');
+    string.addEventListener(
+      gapi.drive.realtime.EventType.TEXT_INSERTED,
+      Overdrive.sendEvent
+    );
+    string.addEventListener(
+      gapi.drive.realtime.EventType.TEXT_DELETED,
+      Overdrive.sendEvent
+    );
+    gapi.drive.realtime.databinding.bindString(
+      string,
+      document.getElementById('editor')
+    );
+    Overdrive.send({
+      type: 'file_content_loaded',
+      text: string.getText()
     });
-  });
-}
+  },
 
-function auth(userId) {
-  gapi.load('auth:client', function() {
-    gapi.auth.authorize({
-      client_id: CLIENT_ID,
-      response_type: 'token',
-      scope: [
-        rtclient.INSTALL_SCOPE,
-        rtclient.FILE_SCOPE,
-        rtclient.OPENID_SCOPE
-      ],
-      immediate: false
-    }, function(token) {
-      if (token) {
-        send({
-          type: 'authorized',
-          token: {
-            access_token: token.access_token
-          }
-        });
-      }
+  create: function(title, userId, accessToken) {
+    gapi.load('auth:client', function() {
+      gapi.auth.setToken({access_token: accessToken});
+      rtclient.createRealtimeFile(title, rtclient.REALTIME_MIMETYPE, function(file) {
+        if (file && file.id) {
+          Overdrive.open(file.id, userId, accessToken);
+        } else {
+          Overdrive.send({
+            type: 'error',
+            error: 'Failed to create file'
+          });
+        }
+      });
     });
-  });
+  },
+
+  open: function(fileId, userId, accessToken) {
+    var options = {
+      clientId: Overdrive.CLIENT_ID,
+      initializeModel: Overdrive.initializeModel,
+      autoCreate: false,
+      newFileMimeType: null, // Using default.
+      onFileLoaded: Overdrive.onFileLoaded,
+      registerTypes: null, // No action.
+      afterAuth: null // No action.
+    };
+
+    var rtLoader = new rtclient.RealtimeLoader(options);
+    // Setting this here because I'm too lazy to update realtime-client-utils.js
+    rtclient.params.fileIds = fileId;
+    rtclient.params.userId = userId;
+
+    gapi.load('auth:client,drive-realtime,drive-share', function() {
+      gapi.auth.setToken({access_token: accessToken});
+      rtLoader.load();
+      rtclient.getFileMetadata(fileId, function(metadata) {
+        if (metadata.error) {
+          Overdrive.send({
+            type: 'error',
+            error: 'File not found'
+          });
+        } else {
+          Overdrive.send({
+            type: 'file_metadata_loaded',
+            metadata: metadata
+          });
+        }
+      });
+    });
+  },
+
+  auth: function(userId) {
+    gapi.load('auth:client', function() {
+      gapi.auth.authorize({
+        client_id: Overdrive.CLIENT_ID,
+        response_type: 'token',
+        scope: [
+          rtclient.INSTALL_SCOPE,
+          rtclient.FILE_SCOPE,
+          rtclient.OPENID_SCOPE
+        ],
+        immediate: false
+      }, function(token) {
+        if (token) {
+          Overdrive.send({
+            type: 'authorized',
+            token: {
+              access_token: token.access_token
+            }
+          });
+        }
+      });
+    });
+  }
+
 }
